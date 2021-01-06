0c91f2fd43f72fce6154b4600b0a3b8cab45a89c
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-06 09:49:46 +0900

    [fix]dbディレクトリに分けて書いていたがうまく適応されなかったのでmainに移動させました。dbディレクトリは今後使うかもしれないのでコメント化して残しておきます。

--- src/db.rs
+++ src/db.rs
@@ -1,41 +1,41 @@
-use crate::models::{History, HistoryEntry};
-use crate::schema::*;
+// use crate::models::{History, HistoryEntry};
+// use crate::schema::*;
 
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
-use dotenv::dotenv;
-use std::env;
+// use diesel::pg::PgConnection;
+// use diesel::prelude::*;
+// use dotenv::dotenv;
+// use std::env;
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-}
+// pub fn establish_connection() -> PgConnection {
+//     dotenv().ok();
+//     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+// }
 
-pub fn create_post<'a>(conn: &PgConnection, input: &'a str /* , done: &'a bool */) -> History {
-    // use schema::*;
-    let history_entory = HistoryEntry { input, /* done */ };
-    diesel::insert_into(history::table)
-        .values(&history_entory)
-        .get_result(conn)
-        .expect("Error saving new post")
-}
+// pub fn create_post<'a>(conn: &PgConnection, input: &'a str /* , done: &'a bool */) -> History {
+//     // use schema::*;
+//     let history_entory = HistoryEntry { input, /* done */ };
+//     diesel::insert_into(history::table)
+//         .values(&history_entory)
+//         .get_result(conn)
+//         .expect("Error saving new post")
+// }
 
-pub fn output() {
-    use crate::diesel::prelude::*;
-    use crate::schema::history::dsl::*;
+// pub fn output() {
+//     use crate::diesel::prelude::*;
+//     use crate::schema::history::dsl::*;
 
-    let connection = establish_connection();
-    let results = history
-        .filter(done.eq(true))
-        .limit(5)
-        .load::<History>(&connection)
-        .expect("Error loading posts");
+//     let connection = establish_connection();
+//     let results = history
+//         .filter(done.eq(true))
+//         .limit(5)
+//         .load::<History>(&connection)
+//         .expect("Error loading posts");
 
-    println!("Displaying {} posts", results.len());
-    for post in results {
-        println!("{}", post.input);
-        println!("----------\n");
-        // println!("{}", post.body);
-    }
-}
+//     println!("Displaying {} posts", results.len());
+//     for post in results {
+//         println!("{}", post.input);
+//         println!("----------\n");
+//         // println!("{}", post.body);
+//     }
+// }
