3c7e5ca1e3e019b13b3e886dea399a507decc596
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-11 03:36:30 +0900

    [add]データベースに値を挿入する（仮）

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,38 +1,15 @@
 use crate::db::*;
 use crate::errors::MyError;
 use crate::models::{AddHistory, IndexTemplate};
-use actix_web::{http::header, web, HttpResponse, Responder};
+use actix_web::{http::header, HttpResponse, Responder};
 use askama::Template;
-
-// pub async fn manual_hello() -> impl Responder {
-//     HttpResponse::Ok().body("hello world")
-// }
-
-// pub fn history() {
-//     let connection = establish_connection();
-//     // let test = "hello";
-//     fn output() -> &str {
-//         let hello = "hello";
-//         return &hello;
-//     }
-
-//     let input = output();
-//     // let test = AddHistory { input };
-
-//     return create_post(&connection, input);
-//     // Ok(HttpResponse::SeeOther()
-//     //     .header(header::LOCATION, "/")
-//     //     .finish())
-// }
 
 pub async fn add_history() -> Result<impl Responder, MyError> {
     let connection = establish_connection();
-    // let test = AddHistory { input: "hello" };
-    let test = "hello";
-    // let input = AddHistory { input: "hello" };
-    // let input = { input: "hello" };
+    let input = "CS:GO";
     // let test = AddHistory { input };
-    create_post(&connection, test);
+    // let test = AddHistory { input: "hello" };
+    create_post(&connection, input);
     Ok(HttpResponse::SeeOther()
         .header(header::LOCATION, "/")
         .finish())
