67b8c65b66f495ebd4151aaf02178e12744daafa
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-14 11:26:05 +0900

    [add]フォームに入力された文字を受け取る処理（仮）

--- src/errors.rs
+++ src/errors.rs
@@ -2,9 +2,9 @@ use actix_web::ResponseError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum MyError {
+pub enum AppError {
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
 }
 
-impl ResponseError for MyError {}
+impl ResponseError for AppError {}
--- src/handlers.rs
+++ src/handlers.rs
@@ -1,21 +1,20 @@
 use crate::db::*;
-use crate::errors::MyError;
+use crate::errors::AppError;
 use crate::models::{AddHistory, IndexTemplate};
-use actix_web::{http::header, web, HttpResponse, Responder};
+
+use actix_web::{get, http::header, web, HttpResponse, Responder};
 use askama::Template;
 
-pub async fn add_history() -> Result<impl Responder, MyError> {
-    let connection = establish_connection();
-    let input = "Hello World!";
-    // let test = AddHistory { input };
-    // let test = AddHistory { input: "hello" };
-    create_post(&connection, input);
-    Ok(HttpResponse::SeeOther()
-        .header(header::LOCATION, "/")
-        .finish())
-}
+// pub async fn add_history() -> Result<impl Responder, AppError> {
+//     let connection = establish_connection();
+//     let input = "Hello World!";
+//     create_post(&connection, input);
+//     Ok(HttpResponse::SeeOther()
+//         .header(header::LOCATION, "/")
+//         .finish())
+// }
 
-pub async fn index() -> Result<impl Responder, MyError> {
+pub async fn index() -> Result<impl Responder, AppError> {
     let entries = show_history();
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
@@ -24,16 +23,11 @@ pub async fn index() -> Result<impl Responder, MyError> {
         .body(response_body))
 }
 
-// pub async fn add_history(params: web::Form<AddHistory<'a>>) -> Result<impl Responder, MyError> {
-//     let connection = establish_connection();
-//     let content = &params.input;
-//     // connection.get_result("INSERT INTO todo (text) VALUES (?)", &[&params.text]);
-//     create_post(&connection, content);
-//     // let responce_history = create_post(&connection, content);
-//     Ok(HttpResponse::SeeOther()
-//         .header(header::LOCATION, "/")
-//         .finish())
-//     // Ok(HttpResponse::Ok()
-//     //     .content_type("application")
-//     //     .body(responce_history))
-// }
+pub async fn add_history(params: web::Form<AddHistory<'_>>) -> Result<impl Responder, AppError> {
+    let connection = establish_connection();
+    let content = &params.input;
+    create_post(&connection, content);
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
