1f8d66b3ab209c6e604673743fccacc7ab0b28dd
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-14 20:28:13 +0900

    [fix]不要なclone()を削除

--- src/handlers.rs
+++ src/handlers.rs
@@ -6,7 +6,7 @@ use askama::Template;
 
 pub async fn add_history(form: web::Form<AddHistory<'_>>) -> Result<impl Responder, AppError> {
     let connection = establish_connection();
-    let input = form.input.clone();
+    let input = form.input;
     create_post(&connection, input);
     Ok(HttpResponse::SeeOther()
         .header(header::LOCATION, "/")
