d29857557df99e121029a0e3fa979df080e2acee
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-18 10:01:06 +0900

    [add]テキストボックスに入力された文字をデータベースに保存しつつ、カスタムサーチのページ遷移もするように変更

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
-function searchGoogle(q) {
-    document.forms['searchForm'].elements['q'].value = q;
-    document.forms['searchForm'].submit();
-    return false;
+// function add() {
+//     let form = document.getElementById(searchButton);
+//     console.log(form);
+// }
+
+function hoge(a, b) {
+    console.log('hoge!');
+    return [a, b];
 }
 
-function selectId() {
-    document.querySelector(input).value;
+function afterHoge() {
+    console.log('after hoge!');
+}
+
+// hogeが実行された後にafterHogeを実行する関数を生成
+var newHoge = callAfter(hoge, afterHoge);
+
+console.log(newHoge('1', '2'));
+// 出力
+// > hoge!
+// > after hoge!
+// > [ '1', '2' ]
+
+function add() {
+    // document.getElementById(customSearch);
+    document.forms['searchForm'].submit();
+}
+
+// let google = function (y) {
+//     let
+// }
+
+function addHistory() {
+    document.forms['addHistory'].submit();
+}
+
+function customSearch() {
+    document.forms['customSearch'].submit();
+}
+
+
+
+// acc.forEach(function(elem, index) {
+//     return (index)
+// })
+// return [addHistory(), customSearch()]
+
+
+function addHistory() {
+    document.forms['addHistory'].submit();
+}
+
+function customSearch() {
+    document.forms['customSearch'].submit();
+}
+
+function callAfter() {
+    let acc = [addHistory(), customSearch()]
+    while (acc.length > 0) {
+        acc.shift();
+    }
 }
