dac823f432a1b17a13462c8420ca633d0e441e61
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-20 14:45:09 +0900

    [clean]HTMLに直接書かれていたjsをjsファイルに書き写してHTMLで読み込むようにしました。

--- index.js
+++ index.js
@@ -1,61 +1,15 @@
-// function add() {
-//     let form = document.getElementById(searchButton);
-//     console.log(form);
-// }
-
-function hoge(a, b) {
-    console.log('hoge!');
-    return [a, b];
-}
-
-function afterHoge() {
-    console.log('after hoge!');
-}
-
-// hogeが実行された後にafterHogeを実行する関数を生成
-var newHoge = callAfter(hoge, afterHoge);
-
-console.log(newHoge('1', '2'));
-// 出力
-// > hoge!
-// > after hoge!
-// > [ '1', '2' ]
-
-function add() {
-    // document.getElementById(customSearch);
-    document.forms['searchForm'].submit();
-}
-
-// let google = function (y) {
-//     let
-// }
-
 function addHistory() {
     document.forms['addHistory'].submit();
 }
 
 function customSearch() {
-    document.forms['customSearch'].submit();
-}
-
-
-
-// acc.forEach(function(elem, index) {
-//     return (index)
-// })
-// return [addHistory(), customSearch()]
-
-
-function addHistory() {
-    document.forms['addHistory'].submit();
-}
-
-function customSearch() {
+    let input_text = document.getElementById("input").value;
+    document.forms['customSearch'].elements['q'].value = input_text;
     document.forms['customSearch'].submit();
 }
 
 function callAfter() {
-    let acc = [addHistory(), customSearch()]
+    let acc = [customSearch(), addHistory()]
     while (acc.length > 0) {
         acc.shift();
     }
