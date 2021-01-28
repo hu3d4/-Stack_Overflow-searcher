1ceb152af930775dea3c8bf97fae2a129d35fc31
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-29 03:19:33 +0900

    [add]DbErrorを追加しました

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,10 @@
-use actix_web::ResponseError;
+use actix_web::error::ResponseError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum AppError {
+    #[error("Failed to get connection")]
+    DbError(diesel::result::Error),
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
 }
