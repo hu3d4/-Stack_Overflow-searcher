26091c7ccbf40dbceda2292938303d70bae981c9
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-29 10:05:29 +0900

    [update]Pgコネクションをパブリックファンクションからプライベートファンクションに変更

--- src/db.rs
+++ src/db.rs
@@ -7,7 +7,7 @@ use diesel::prelude::{Connection, ExpressionMethods, QueryDsl, RunQueryDsl};
 use dotenv::dotenv;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
+fn establish_connection() -> PgConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
