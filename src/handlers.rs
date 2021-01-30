4eb6e9ffe44dd4f297a0fda0b223abb9faae37b6
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-01-31 01:42:01 +0900

    [add]一つだけ履歴を消す、delete_one_historyファンクションを設定

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,6 @@
 use crate::db;
 use crate::errors::AppError;
+use crate::models::DeleteHistory;
 use crate::models::{AddHistory, IndexTemplate};
 use actix_web::{http::header, web, HttpResponse, Responder};
 use askama::Template;
@@ -23,6 +24,16 @@ pub async fn add_history(form: web::Form<AddHistory>) -> Result<impl Responder, 
 
 pub async fn delete_history() -> Result<impl Responder, AppError> {
     db::delete_history()?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
+pub async fn delete_one_history(
+    form: web::Form<DeleteHistory>,
+) -> Result<impl Responder, AppError> {
+    let id = form.id;
+    db::delete_one_history(id);
     Ok(HttpResponse::SeeOther()
         .header(header::LOCATION, "/")
         .finish())
