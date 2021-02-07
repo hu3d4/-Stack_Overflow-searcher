2cd325df6466340a1b762a39e081ec29ca05a8c2
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-08 06:57:52 +0900

    [update]bindを削除してTcpListenerに変更しました。

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ extern crate dotenv;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("unable to bind TCP listener");
+    let listener = TcpListener::bind("0.0.0.0:8080").expect("unable to bind TCP listener");
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
@@ -23,7 +23,6 @@ async fn main() -> std::io::Result<()> {
             .route("/delete_one", web::post().to(delete_one_history))
     })
     .listen(listener)?
-    // .bind("0.0.0.0:8080")?
     .run()
     .await
 }
