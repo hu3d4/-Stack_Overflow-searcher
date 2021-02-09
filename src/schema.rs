58e56bac65dbf0afd7fa2e60916eb38163ca2df7
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-09 13:28:45 +0900

    [update]historyからhistoriesに名前を変更しました.

--- src/schema.rs
+++ src/schema.rs
@@ -1,7 +1,20 @@
 table! {
-    history (id) {
+    histories (id) {
         id -> Int4,
         input -> Text,
         done -> Bool,
     }
 }
+
+table! {
+    users (id) {
+        id -> Int4,
+        email -> Text,
+        pw -> Text,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    histories,
+    users,
+);
