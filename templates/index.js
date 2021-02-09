c97ea5cca8921cec3b59ec9dd85bb42d95445fd5
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-09 19:13:54 +0900

    [temporary]js,html,cssのファイルを分けました。

---
+++ templates/index.js
@@ -0,0 +1,17 @@
+function addHistory() {
+    document.forms['addHistory'].submit();
+}
+
+function customSearch() {
+    let input_text = document.getElementById("input").value;
+    document.forms['customSearch'].elements['q'].value = input_text;
+    document.forms['customSearch'].submit();
+}
+
+function callAfter() {
+    console.log("fjei")
+    let acc = [customSearch(), addHistory()]
+    while (acc.length > 0) {
+        acc.shift();
+    }
+}
