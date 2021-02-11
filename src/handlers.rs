4e14a121de985fc7762437384126f67dcbea9fda
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-11 11:56:08 +0900

    [update]delete_one_historyからdelete_single_historyにファンクションの名前を変更しました。

--- src/handlers.rs
+++ src/handlers.rs
@@ -7,10 +7,44 @@ use askama::Template;
 pub async fn index() -> Result<impl Responder, AppError> {
     let entries = db::show_history()?;
     let user = db::show_user();
-    for i in entries.iter() {
-        let a = i.id;
-    }
-    let html = IndexTemplate { entries };
+
+    let html = for u in user.iter() {
+        for i in u.iter() {
+            let e = i.id;
+            for h in entries.iter() {
+                let a = h.id;
+                if e == a {
+                    IndexTemplate { entries }
+                } else {
+                    panic!("jfie");
+                };
+            }
+        }
+    };
+    // let html = for u in user.iter() {
+    //     for i in u.iter() {
+    //         let e = i.id;
+    //         for h in entries.iter() {
+    //             let a = h.id;
+    //             if e == a {
+    //                 IndexTemplate { entries }
+    //             } else {
+    //                 panic!("jfie");
+    //             };
+    //         }
+    //     }
+    // };
+
+    // let history_id = for h in entries.iter() {
+    //     let a = h.id;
+    // };
+
+    // let html = match user_id {
+    //     history_id => IndexTemplate { entries },
+    //     _ => panic!("fjie"),
+    // };
+
+    // let html = IndexTemplate { entries };
     let response_body = html.render()?;
     Ok(HttpResponse::Ok()
         .content_type("text/html")
@@ -56,11 +90,11 @@ pub async fn delete_history() -> Result<impl Responder, AppError> {
         .finish())
 }
 
-pub async fn delete_one_history(
+pub async fn delete_single_history(
     form: web::Form<DeleteHistory>,
 ) -> Result<impl Responder, AppError> {
     let id = form.id;
-    db::delete_one_history(id)?;
+    db::delete_single_history(id)?;
     Ok(HttpResponse::SeeOther()
         .header(header::LOCATION, "/")
         .finish())
