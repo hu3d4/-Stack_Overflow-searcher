184b3f0c63c7cf13250329c55973efe0314677ab
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-13 01:47:10 +0900

    [update]emailを削除しuserをユニークな値にしました。

--- migrations/2021-02-09-031524_user/up.sql
+++ migrations/2021-02-09-031524_user/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL,
-    user TEXT NOT NULL UNIQUE,
+    users TEXT NOT NULL UNIQUE,
     login_status BOOLEAN NOT NULL DEFAULT true,
     PRIMARY KEY (id)
 );
