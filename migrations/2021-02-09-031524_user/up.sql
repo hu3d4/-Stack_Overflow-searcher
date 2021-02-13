78b80c4b01a766f0175aaef72779c4663d9a581a
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-13 20:03:48 +0900

    [fix]useridに名前を変更しました。

--- migrations/2021-02-09-031524_user/up.sql
+++ migrations/2021-02-09-031524_user/up.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS users (
     userid SERIAL,
     username TEXT NOT NULL UNIQUE,
     login_status BOOLEAN NOT NULL DEFAULT true,
-    PRIMARY KEY (id)
+    PRIMARY KEY (userid)
 );
