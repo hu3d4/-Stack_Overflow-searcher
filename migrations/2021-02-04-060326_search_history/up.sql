4210ecdb958e933bb3a1d4928c3ec3670451b7c5
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-13 19:57:38 +0900

    [fix],をつけました。

--- migrations/2021-02-04-060326_search_history/up.sql
+++ migrations/2021-02-04-060326_search_history/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS histories (
     id SERIAL,
-    userid INTEGER
+    userid INTEGER,
     input TEXT NOT NULL,
     done BOOLEAN NOT NULL DEFAULT true,
     PRIMARY KEY (id)
