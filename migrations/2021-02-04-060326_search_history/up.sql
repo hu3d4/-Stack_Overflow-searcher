57b432d8e8a1d44e2722a153df5e7b7cfa2c10c9
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-13 21:55:32 +0900

    [add]useridを追加しました。

--- migrations/2021-02-04-060326_search_history/up.sql
+++ migrations/2021-02-04-060326_search_history/up.sql
@@ -1,6 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS history (
     id SERIAL,
+    userid INTEGER NOT NULL,
     input TEXT NOT NULL,
     done BOOLEAN NOT NULL DEFAULT true,
     PRIMARY KEY (id)
