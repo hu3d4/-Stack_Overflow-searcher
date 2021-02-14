8b9253a1212db352efb23e31b85fd51ccdec0dc2
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-15 02:42:47 +0900

    [update]usernameのコメントアウトを解除しました。

--- migrations/2021-02-04-060326_search_history/up.sql
+++ migrations/2021-02-04-060326_search_history/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS histories (
     id SERIAL,
-    -- username TEXT NOT NULL,
+    username TEXT NOT NULL,
     input TEXT NOT NULL,
     done BOOLEAN NOT NULL DEFAULT true,
     PRIMARY KEY (id)
