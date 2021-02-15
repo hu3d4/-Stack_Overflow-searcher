80e37df838e8d26b92c5e9d5e01197da5e1efc97
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-15 17:20:10 +0900

    [add]usernameを表示するための構成にしました。

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,11 @@ async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8080").expect("unable to bind TCP listener");
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(index))
+            // .route("/", web::get().to(index))
             .route("/get_history", web::post().to(get_history))
             .route("/get_user", web::post().to(get_user))
-            .route("/get_user/{username}", web::get().to(index))
+            .route("/get_user/{username}", web::get().to(index_user))
+            // .route("/get_user/{username}", web::get().to(index))
             // .route("/get_user/{username}", web::get().to(username_html))
             // .route("/get_user{_:/?}/main{_:/?}", web::get().to(index))
             .route("/delete", web::post().to(delete_history))
