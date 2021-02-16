462f928370e5827c66a6b28c5bdb53d2a55c4d88
hu3d4 <66501033+hu3d4@users.noreply.github.com>
2021-02-16 15:12:19 +0900

    [clean]インデントと改行を整えました。

--- src/db.rs
+++ src/db.rs
@@ -43,9 +43,11 @@ pub fn show_history() -> Result<Vec<History>, AppError> {
 
 pub fn delete_all_history(username: String) -> Result<usize, AppError> {
     let conn = establish_connection();
-    return diesel::delete(histories::table.filter(histories::username.eq(format!("{}", username))))
-        .execute(&conn)
-        .map_err(|e| (AppError::DbError(e)));
+    return diesel::delete(
+        histories::table.filter(histories::username.eq(format!("{}", username))),
+    )
+    .execute(&conn)
+    .map_err(|e| (AppError::DbError(e)));
 }
 
 pub fn delete_one_history(id: i32) -> Result<usize, AppError> {
